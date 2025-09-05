fea51f42fb188513ac8f7a4b0137c4fbfad8b19b
bluek <bluek1000@gmail.com>
2025-09-05 21:24:30 +0900

    Initial Commit

---
+++ bubbleSkyTests/bubbleSkyTests.swift
@@ -0,0 +1,17 @@
+//
+//  bubbleSkyTests.swift
+//  bubbleSkyTests
+//
+//  Created by sang gi kim on 9/5/25.
+//
+
+import Testing
+@testable import bubbleSky
+
+struct bubbleSkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
